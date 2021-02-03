40e08e239521bedcfa107a6bb7f7bb8207d4f864
all-an <all.ancapistan@gmail.com>
2021-02-03 10:20:55 -0300

    learning

---
+++ 1_0_4_part4/src/VariablesTest.java
@@ -0,0 +1,14 @@
+public class VariablesTest {
+	public static void main(String[] args) {
+		System.out.println("hi new test");
+		
+		int age; 
+		age = 37;
+		
+		System.out.println(age);
+		
+		age = 40 + 7;
+		
+		System.out.println(age);
+	}
+}
