ad5081a17360d2e9fd720416e5ad14fe03a100e4
all-an <all.ancapistan@gmail.com>
2021-03-17 14:42:41 -0300

    learning

--- 1_0_1 Part 1/maiteiyvora/src/maiteiyvora/Maiteiyvora.java
+++ 1_0_1 Part 1/maiteiyvora/src/maiteiyvora/Maiteiyvora.java
@@ -3,5 +3,26 @@ package maiteiyvora;
 public class Maiteiyvora {
 	public static void main(String[] args) {
 		System.out.println("Maitei Yvóra");
+		
+		double salario = 1270.50;
+		int valor = (int) salario;
+		
+		//double valor = 3;
+		System.out.println(valor);
+	
+		long numeroGrande = 2352352523L;
+		short valorPequeno = 2131;
+		byte b = 127;
+	
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+		
+		float pontoFlutuante = 3.14f;
+		
+		System.out.println(pontoFlutuante);
+		
 	}
 }
