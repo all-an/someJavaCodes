68c3a2df4175a41abfff568b190220c084b42639
all-an <all.ancapistan@gmail.com>
2021-03-18 10:00:03 -0300

    learning

---
+++ 1_0_1 Part 1/TestaCaracteres/src/module-info.java
@@ -0,0 +1,2 @@
+module testaCaracteres {
+}
---
+++ 1_0_1 Part 1/testacaractere/src/testacaractere/TestaCaractere.java
@@ -0,0 +1,20 @@
+package testacaractere;
+
+public class TestaCaractere {
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String palavra = "testando a String";
+		
+		palavra = palavra + 2020;
+		System.out.println(palavra);
+	}
+
+}
